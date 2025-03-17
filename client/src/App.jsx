95728d4cbd8448fafd1466604696ef07a4c1e265
Drew <tullos.andrew@gmail.com>
2025-03-17 12:19:12 -0500

    UI Canvas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { Button } from "./components/ui/button";
 
 function App() {
 	return (
-		<div className="flex items-center justify-center h-screen bg-amber-600">
+		<div className="flex items-center justify-center h-screen bg-transparent">
 			<Button>Hello World</Button>
 		</div>
 	);
