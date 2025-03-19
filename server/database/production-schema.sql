74ad14ddb6c3fc5a0e4aeb1f647540bacff4c6ea
Drew <tullos.andrew@gmail.com>
2025-03-19 13:36:36 -0500

    Schema Test Db Reconfigure

--- server/database/production-schema.sql
+++ server/database/production-schema.sql
@@ -1,36 +1,64 @@
-drop database if exists solar;
-create database solar;
-use solar;
+drop database if exists rails_app_db;
+create database rails_app_db;
+use rails_app_db;
 
-create table `user` (
-	user_id int primary key auto_increment,
-    username varchar(255) unique,
-    `password` text
+CREATE TABLE IF NOT EXISTS `user_profile` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`username` varchar(100) NOT NULL,
+	`email` varchar(255) NOT NULL UNIQUE,
+	`password` varchar(255) NOT NULL,
+	`profile_picture` varchar(255) NULL,
+	`first_name` varchar(100) NOT NULL,
+	`last_name` varchar(100) NOT NULL,
+	`city` varchar(255) NOT NULL,
+	`state` varchar(50) NOT NULL,
+	`postal_code` varchar(10) NOT NULL,
+	`created_at` datetime NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
-create table panel (
-	panel_id int primary key auto_increment,
-    section varchar(50) not null,
-    `row` int not null,
-    `column` int not null,
-    year_installed int not null,
-    material varchar(50) not null,
-    is_tracking boolean not null,
-    user_id int not null,
-    foreign key (user_id) references `user`(user_id),
-    constraint uq_section_row_column
-		unique(section, `row`, `column`)
+CREATE TABLE IF NOT EXISTS `user_clip` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`user_id` int NOT NULL,
+	`media_url` text NOT NULL,
+	`caption` varchar(255) NOT NULL,
+	`created_at` datetime NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
-insert into `user` (username, `password`)
-	values
-    ('test username 1', 'password1'),
-	('test username 2', 'password2');
+CREATE TABLE IF NOT EXISTS `comment` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`comment_user_id` int NOT NULL,
+	`clip_id` int NOT NULL,
+	`text` varchar(255) NOT NULL,
+	`created_at` datetime NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
-insert into panel (section, `row`, `column`, year_installed, material, is_tracking, user_id)
-		values
-        ('The Ridge', 1, 1, 2020, 'POLY_SI', true, 1),
-        ('The Ridge', 1, 2, 2020, 'CD_TE', true, 2),
-        ('The Ridge', 1, 3, 2020, 'MONO_SI', true, 2),
-        ('Flats', 4, 1, 2020, 'A_SI', false, 1),
-        ('Flats', 5, 1, 2020, 'MONO_SI', false, 2);
+CREATE TABLE IF NOT EXISTS `like` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`user_id` int NOT NULL,
+	`clip_id` int NOT NULL,
+	`created_at` datetime NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `follow` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`follower_id` int NOT NULL,
+	`followee_id` int NOT NULL,
+	`created_at` datetime NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+
+ALTER TABLE `user_clip` ADD CONSTRAINT `user_clip_fk1` FOREIGN KEY (`user_id`) REFERENCES `user_profile`(`id`);
+ALTER TABLE `comment` ADD CONSTRAINT `comment_fk1` FOREIGN KEY (`comment_user_id`) REFERENCES `user_profile`(`id`);
+
+ALTER TABLE `comment` ADD CONSTRAINT `comment_fk2` FOREIGN KEY (`clip_id`) REFERENCES `user_clip`(`id`);
+ALTER TABLE `like` ADD CONSTRAINT `like_fk1` FOREIGN KEY (`user_id`) REFERENCES `user_profile`(`id`);
+
+ALTER TABLE `like` ADD CONSTRAINT `like_fk2` FOREIGN KEY (`clip_id`) REFERENCES `user_clip`(`id`);
+ALTER TABLE `follow` ADD CONSTRAINT `follow_fk1` FOREIGN KEY (`follower_id`) REFERENCES `user_profile`(`id`);
+
+ALTER TABLE `follow` ADD CONSTRAINT `follow_fk2` FOREIGN KEY (`followee_id`) REFERENCES `user_profile`(`id`);
