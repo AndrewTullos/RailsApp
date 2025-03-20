95746f3682bb8c0cfe00a4264e3e2ed96b917f81
Drew <tullos.andrew@gmail.com>
2025-03-20 12:41:10 -0500

    Last Commit Before Rewriting AWS

--- server/src/main/java/org/rails/controllers/FileUploadController.java
+++ server/src/main/java/org/rails/controllers/FileUploadController.java
@@ -6,11 +6,13 @@ import org.rails.data.FileUploadResponse;
 import org.rails.domain.FileService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@CrossOrigin
 @RestController
 @Slf4j
 @RequestMapping("/api/v1/s3")
